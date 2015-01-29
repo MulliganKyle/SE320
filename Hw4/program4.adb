9138e07c66310810030bf688b9f7d15d403c8373
MulliganKyle <Mulligan.Kyle@gmail.com>
2015-01-28 18:40:17 -0700

    change Hw4

--- Hw4/program4.adb
+++ Hw4/program4.adb
@@ -10,15 +10,23 @@
 with ada.text_io, ada.integer_text_io, primeAndFactor;
 
 procedure program4 is
+   Not_Prime: exception;
    userInput: Integer range -20..50;
    done: boolean :=false;
 begin
    
-   while  done=false loop
-      
+   while not done loop
+
+      begin
       ada.text_io.put(" Enter a prime number between -20 and +50: ");
       ada.integer_text_io.get(userInput);
       done:=true;
+
+
+
+
+
+      end;
 
    end loop;
 end program4;
